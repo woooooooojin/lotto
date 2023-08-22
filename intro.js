c53333c007bcfab4d6085744d10bb73eae5fabb2
woooooooojin <kevin2205@naver.com>
2023-08-22 16:12:10 +0900

    add intro

---
+++ intro.js
@@ -0,0 +1,11 @@
+let counting = document.querySelector('.counting')
+let cnt = 0;
+
+let count = setInterval(() => {
+    if(cnt === 100){
+        clearInterval(count)
+        return false;
+    }
+    cnt += 1
+    counting.innerHTML = cnt + '%'
+}, 20);
